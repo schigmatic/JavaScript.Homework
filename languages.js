1115bd885448644723f8735d3b4997d0ff908fe2
osska <makeeva_alyona228@mail.ru>
2026-02-20 18:35:03 +0300

    управление потоками

---
+++ languages.js
@@ -0,0 +1,20 @@
+const lang = "ru";
+switch (lang) {
+  case "ru":
+    console.log("Добрый день!");
+    break;
+  case "en":
+    console.log("Good afternoon!");
+    break;
+  case "de":
+    console.log("Guten Tag!");
+    break;
+  case "fr":
+    console.log("Bonjour!");
+    break;
+  case "hi":
+    console.log("Shubh din!");
+    break;
+  default:
+    console.log("Error!");
+}
