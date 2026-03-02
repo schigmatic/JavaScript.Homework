226f5bd1caa430fcc1aae82b14bd4ed9b67637ae
osska <schigmatic@gmail.com>
2026-03-02 14:39:42 +0300

    Сортировка массива

---
+++ 9-sort-loops/9-sort-loops.js
@@ -0,0 +1,17 @@
+const arr = [1, 40, -5, 10, 0];
+function sortEven(arr) {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      result.push(arr[i]);
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
+const sortedArr = sortEven(arr);
+console.log(sortedArr);
